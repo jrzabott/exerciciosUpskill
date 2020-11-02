ffc13aa7f3fa8177b1356cee71dec2ee45ea50b9
Daniel Junior <jrzabott@gmail.com>
2020-11-02 21:32:49 +0000

    added Javadoc to ComplexNumber

--- POO/20201102-ExercicioNumerosComplexos/src/main/java/upskill/exercicionumeroscomplexos/NumeroComplexo.java
+++ POO/20201102-ExercicioNumerosComplexos/src/main/java/upskill/exercicionumeroscomplexos/NumeroComplexo.java
@@ -7,98 +7,172 @@ package upskill.exercicionumeroscomplexos;
 
 /**
  *
- * @author user
+ * @author Daniel Junior
+ * @version 0.1
+ * This Class was created as an exercise to handle complex numbers.
  */
 public class NumeroComplexo {
     
     private double parteReal;
     private double parteImaginaria;
     
+    /**
+     *
+     * @param parteReal
+     * @param parteImaginaria
+     */
     public NumeroComplexo(double parteReal, double parteImaginaria) {
         this.parteReal = parteReal;
         this.parteImaginaria = parteImaginaria;
     }
     
+    /**
+     * Zero param Constructor
+     */
     public NumeroComplexo() {
     }
     
+    /**
+     *
+     * @return Real part of the number
+     */
     public double getParteReal() {
         return parteReal;
     }
     
+    /**
+     *
+     * @return Imaginary part of a complex number
+     */
+    public double getParteImaginaria() {
+        return parteImaginaria;
+    }
+    /**
+     *
+     * @param parteReal
+     */
     public void setParteReal(double parteReal) {
         this.parteReal = parteReal;
     }
     
-    public double getParteImaginaria() {
-        return parteImaginaria;
-    }
     
+    /**
+     *
+     * @param parteImaginaria
+     */
     public void setParteImaginaria(double parteImaginaria) {
         this.parteImaginaria = parteImaginaria;
     }
     
     @Override
     public String toString() {
-        return String.format("NumeroComplexo{Numero complexo = %.2f + (i * %.2f)",parteReal,parteImaginaria );
+        return String.format("NumeroComplexo{Numero complexo = %.2f + (i * %.2f)", getParteReal(), getParteImaginaria());
     }
     
+    /**
+     *
+     * @return true if the number has no Real part.
+     */
     public boolean isReal() {
-        return (parteImaginaria == 0);
+        return (getParteImaginaria() == 0);
     }
     
+    /**
+     *
+     * @return trus if the number has no Imaginary part
+     */
     public boolean isImaginario() {
-        return (parteReal == 0);
+        return (getParteReal() == 0);
     }
     
+    /**
+     *
+     * @param n2 and compare if both numbers are equal
+     * @return true if both parts are equal (Real and Imaginary)
+     */
     public boolean isEqual(NumeroComplexo n2) {
-        return (this.parteReal == n2.parteReal && this.parteImaginaria == n2.parteImaginaria);
+        return (this.getParteReal() == n2.getParteReal() && this.getParteImaginaria() == n2.getParteImaginaria());
     }
     
+    /**
+     *
+     * @return the module (length of the vector) of a complex number.
+     */
     public double modulo() {
-        return Math.sqrt(Math.pow(parteReal, parteReal) + Math.pow(parteImaginaria, parteImaginaria));
+        return Math.sqrt(Math.pow(getParteReal(), getParteReal()) + Math.pow(getParteImaginaria(), getParteImaginaria()));
     }
     
+    /**
+     *
+     * @return angle in Rad scale.
+     */
     public double argumentosEmRad() {
-        double seno = parteImaginaria / this.modulo();
-        double coseno = parteReal / this.modulo();
+        double seno = getParteImaginaria() / this.modulo();
+        double coseno = getParteReal() / this.modulo();
         return seno / coseno;
     }
     
+    /**
+     *
+     * @return the angle in Degrees
+     */
     public double argumentosEmGraus() {
         return argumentosEmRad() * 180 / Math.PI;
     }
     
+    /**
+     *
+     * @return get the mirrored version of the complex number.
+     */
     public NumeroComplexo getConjugado() {
-        return new NumeroComplexo(parteReal, parteImaginaria * (-1));
+        return new NumeroComplexo(getParteReal(), getParteImaginaria() * (-1));
     }
     
+    /**
+     *
+     * @param n2 is summed with this ComplexNumber
+     * @return a new ComplexNumber
+     */
     public NumeroComplexo soma(NumeroComplexo n2) {
-        return new NumeroComplexo(this.parteReal + n2.parteReal, this.parteImaginaria + n2.parteImaginaria);
+        return new NumeroComplexo(this.getParteReal() + n2.getParteReal(), this.getParteImaginaria() + n2.getParteImaginaria());
     }
     
+    /**
+     *
+     * @param n2 analogue to add
+     * @return
+     */
     public NumeroComplexo subtracao(NumeroComplexo n2) {
-        return new NumeroComplexo(this.parteReal - n2.parteReal, this.parteImaginaria - n2.parteImaginaria);
+        return new NumeroComplexo(this.getParteReal() - n2.getParteReal(), this.getParteImaginaria() - n2.getParteImaginaria());
     }
     
+    /**
+     *
+     * @param n2 return the product of a complex number for another
+     * @return a Complex Number
+     */
     public NumeroComplexo multiplicacao(NumeroComplexo n2) {
-        return new NumeroComplexo((this.parteReal * n2.parteReal - this.parteImaginaria * n2.parteImaginaria), (this.parteReal * n2.parteImaginaria + n2.parteReal * n2.parteReal));
+        return new NumeroComplexo((this.getParteReal() * n2.getParteReal() - this.getParteImaginaria() * n2.getParteImaginaria()), (this.getParteReal() * n2.getParteImaginaria() + n2.getParteReal() * n2.getParteReal()));
     }
     
+    /**
+     *
+     * @param n2 analogue to product.
+     * @return
+     */
     public NumeroComplexo divisao(NumeroComplexo n2) {
         return this.multiplicacao(n2.inversoNumeroComplexo());
     }
     
+    /**
+     *
+     * @return the inverse of a Complex Number (z ^ -1)
+     */
     public NumeroComplexo inversoNumeroComplexo() {
         return new NumeroComplexo(
-                parteReal / (Math.pow(parteReal, 2) 
-                        + Math.pow(parteImaginaria, 2)),
-                (-1 * parteImaginaria) / (Math.pow(parteReal, 2) 
-                        + Math.pow(parteImaginaria, 2)));
+                getParteReal() / (Math.pow(getParteReal(), 2) 
+                        + Math.pow(getParteImaginaria(), 2)),
+                (-1 * getParteImaginaria()) / (Math.pow(getParteReal(), 2) 
+                        + Math.pow(getParteImaginaria(), 2)));
     }
 }
-
-/*
-(a + ib)(c - id)/(c + id)(c - id) = (a + ib)[(c- id)/(c2 + d2)] = z1.z2-1
-
-*/
