05080fce371cc1d377b7fd79bc5b9bed2d223e07
Daniel Junior <danielamrjunior@upskill.pt>
2020-12-08 18:40:48 +0000

    File IO error handling. Try-Catch statements.

---
+++ Java Files/Outros/JavaBeginnersGuide_Chapter8/src/main/java/upskill/javabeginnersguide_chapter8/ShowFile.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package upskill.javabeginnersguide_chapter8;
+
+import java.io.*;
+
+/**
+ *
+ * @author user
+ */
+public class ShowFile {
+    public static void main(String[] args) {
+        int i;
+        FileInputStream fin;
+        
+//        //first make sure that a file has been specified.
+//        if (args.length != 1) {
+//            System.out.println("Usage: ShowFile File");
+//            return;
+//        }
+        try {
+            fin = new FileInputStream("test.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found");
+            return;
+        }
+        
+        try {
+            // read bytes until EOF is encountered
+            do {
+                i = fin.read(); // read from file
+                if (i != -1)
+                    System.out.print((char) i);
+            } while (i != -1); // -1 means EOF
+            
+        } catch (IOException e) {
+            System.out.println("Error reading file.");
+        }
+        
+        try {
+            fin.close(); // close the file
+        } catch (IOException e) {
+            System.out.println("Error closing file.");
+        }
+    }
+}
