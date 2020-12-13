49979c043dbf4f11b44c1d8beda05d2c1975b95b
Daniel Junior <danielamrjunior@upskill.pt>
2020-12-13 15:46:30 +0000

    Added ticktock example on how to use wait() and notify() methods

---
+++ Java Files/Outros/JavaBeginnersGuide_Chapter8/src/main/java/upskill/javabeginnersguide_chapter8/TickTock.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package upskill.javabeginnersguide_chapter8;
+
+/**
+ *
+ * @author user
+ */
+// Use wait() and notify() too reate a ticking clock.
+public class TickTock {
+
+    String state;
+    
+    synchronized void tick(boolean running) {
+        if (!running) { // stops the clock
+            state = "ticked";
+            notify(); // tick() notifies tock()
+            return;
+        }
+        System.out.print("Tick ");
+        
+        state = "ticked"; // Set the current stsate to ticked
+        notify(); // let tock() run
+        
+        try {
+            while(!state.equals("tocked"))
+                wait(); // wait for the tock() to complete <-- tick() waits for tock()
+        } catch (InterruptedException e) {
+            System.out.println("Thread Interrupted.");
+        }
+    }
+    synchronized void tock(boolean running) {
+        if (!running) { // stops the clock
+            state = "tocked";
+            notify(); // tock() notifies tick()
+            return;
+        }
+        System.out.println("Tock");
+        
+        state = "tocked"; // Set the current stsate to ticked
+        notify(); // let tock() run
+        
+        try {
+            while(!state.equals("ticked"))
+                wait(); // wait for the tock() to complete <-- tock() waits for tick()
+        } catch (InterruptedException e) {
+            System.out.println("Thread Interrupted.");
+        }
+    }
+    
+}
