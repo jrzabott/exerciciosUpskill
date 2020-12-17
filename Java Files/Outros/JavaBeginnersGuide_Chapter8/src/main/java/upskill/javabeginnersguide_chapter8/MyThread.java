aecbdf098fa6583c8a260d3be4a2ad001001b925
Daniel Junior <danielamrjunior@upskill.pt>
2020-12-17 14:42:23 +0000

    MyThread edited to support stop, suspend, resume.

--- Java Files/Outros/JavaBeginnersGuide_Chapter8/src/main/java/upskill/javabeginnersguide_chapter8/MyThread.java
+++ Java Files/Outros/JavaBeginnersGuide_Chapter8/src/main/java/upskill/javabeginnersguide_chapter8/MyThread.java
@@ -15,9 +15,11 @@ package upskill.javabeginnersguide_chapter8;
 // a thread.
 public class MyThread implements Runnable {
     Thread myThread;
+    boolean suspended, stopped;
 
-    public MyThread(String threadName) {
-        myThread = new Thread(this, threadName);
+    public MyThread(String name) {
+        myThread = new Thread(this, name);
+        suspended = stopped = true;
     }
 
     // Factory method that creates and start a thread.
@@ -32,10 +34,23 @@ public class MyThread implements Runnable {
         String txt = String.format("%s starting...", myThread.getName());
         System.out.println(txt);
         try {
-            for (int i = 0; i < 10; i++) {
-                Thread.sleep(400);
-                txt = String.format("In %s, count is %s.", myThread.getName(), i);
-                System.out.println(txt);
+            for (int i = 0; i < 1000; i++) {
+                txt = String.format("%d ", i);
+                System.out.print(txt);
+                if (i % 10 == 0) {
+                    System.out.println();
+                    Thread.sleep(250);
+                }
+                
+                // Use synchronized block fo check suspended and stopped.
+                synchronized(this){
+                    while (suspended) {                        
+                        wait();
+                    }
+                    if (stopped) {
+                        break;
+                    }
+                }
             }
         } catch (InterruptedException e) {
             txt = String.format("%s interrupted!", myThread.getName());
@@ -43,7 +58,25 @@ public class MyThread implements Runnable {
         }
         txt = String.format("%s terminating...", myThread.getName());
         System.out.println(txt);
-
     }
 
+    // stop the thread
+    synchronized void myStop(){
+        stopped = true;
+        
+        // ensures a suspended thread can be stopped.
+        suspended = false;
+        notify();
+    }
+    
+    //suspend the thread.
+    synchronized void mySuspend(){
+        suspended = true;
+    }
+    
+    // resume the thread
+    synchronized void myResume(){
+        suspended = false;
+        notify();
+    }
 }
