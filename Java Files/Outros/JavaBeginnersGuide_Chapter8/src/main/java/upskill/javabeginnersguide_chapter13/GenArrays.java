600074a99dbd81d579aaa96dbddc83432a2fe8eb
Daniel Junior <danielamrjunior@upskill.pt>
2020-12-19 14:37:12 +0000

    Generic arrays and how to us/handle them.

---
+++ Java Files/Outros/JavaBeginnersGuide_Chapter8/src/main/java/upskill/javabeginnersguide_chapter13/GenArrays.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package upskill.javabeginnersguide_chapter13;
+
+/**
+ *
+ * @author user
+ */
+public class GenArrays<T extends Number> {
+
+    T ob;
+    T vals[]; // OK
+
+    GenArrays(T o, T nums[]) {
+        ob = o;
+
+        //this is illegal:
+        // vals = new T[10];
+        // however this is legal:
+        vals = nums; // OK to assign ref to existent array
+    }
+}
