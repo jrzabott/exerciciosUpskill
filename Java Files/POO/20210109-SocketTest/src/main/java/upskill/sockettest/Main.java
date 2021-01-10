fb52ccc2151cf6d296c332046e86b9a0b89dd8da
Daniel Junior <danielamrjunior@upskill.pt>
2021-01-10 14:00:59 +0000

    .

--- Java Files/POO/20210109-SocketTest/src/main/java/upskill/sockettest/Main.java
+++ Java Files/POO/20210109-SocketTest/src/main/java/upskill/sockettest/Main.java
@@ -6,6 +6,7 @@
 package upskill.sockettest;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -22,32 +23,41 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String hostname = "www.google.com";
-        int port = 80;
+        String hostname = "localhost";
+        int port = 10000;
 
 //        for (port = 1; port < 100; port++) {
-            final int portThread = port;
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    System.out.println("port " + portThread);
-                    try (
-                            Socket echoSocket = new Socket(hostname, portThread);
-                            PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
-                            BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
-                            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));) {
-                        String userInput;
-                        while ((userInput = stdIn.readLine()).isEmpty()) {
-                            out.println(userInput);
-                            System.out.println("echo: " + in.readLine());
-                        }
-                    } catch (Exception e) {
-                        System.out.println(portThread + " - " + e.getMessage());
-                    }
-                    
+        final int portThread = port;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String[] args = {"10000"};
+                try {
+                    EchoServer.main(args);
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
                 }
-            }).start();
+                
+            }
+        }).start();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
             
+        }
+        
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String[] args = {"localhost", "10000"};
+                try {
+                    EchoClient.main(args);
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        }).start();
 //        }
     }
 
